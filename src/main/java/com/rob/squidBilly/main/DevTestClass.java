abcae523ef5c87d3b192d5f1bd3934a1b1c5c8ca
squidBilly <jymbojymbolaya@hotmail.com>
2023-01-17 10:30:20 +0100

    "First Commit of learning-intellij-git"

---
+++ src/main/java/com/rob/squidBilly/main/DevTestClass.java
@@ -0,0 +1,8 @@
+package com.rob.squidBilly.main;
+
+public class DevTestClass {
+    public static void main(String[] args) {
+        DevClass tom = new DevClass("Tom",1001);
+        System.out.println(tom);
+    }
+}

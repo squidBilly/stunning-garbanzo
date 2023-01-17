c1cd19be16a4ee220604759dc89f6102ded68e7f
squidBilly <jymbojymbolaya@hotmail.com>
2023-01-17 10:35:14 +0100

    "Second Commit of learning-intellij-git"

--- src/main/java/com/rob/squidBilly/main/DevTestClass.java
+++ src/main/java/com/rob/squidBilly/main/DevTestClass.java
@@ -4,5 +4,7 @@ public class DevTestClass {
     public static void main(String[] args) {
         DevClass tom = new DevClass("Tom",1001);
         System.out.println(tom);
+        DevClass jim = new DevClass("Jim",1002);
+        System.out.println(jim);
     }
 }
---
+++ src/main/java/com/rob/squidBilly/main/IntellijGitTest.java
@@ -0,0 +1,8 @@
+package com.rob.squidBilly.main;
+
+public class IntellijGitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git for intellij");
+        System.out.println("Hello Tom and Jom");
+    }
+}
